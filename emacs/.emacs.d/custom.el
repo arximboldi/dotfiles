21120ebb5bbfb4483d83665e8774c4fd0873067f
Juan Pedro Bolivar Puete <raskolnikov@gnu.org>
2015-02-03 11:24:36 +0100

    emacs: move all colors from custom.el to jpb-colors.el and improve them

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -106,18 +106,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background "#000000" :foreground "white" :height 150 :family "Inconsolata"))))
- '(custom-button ((t (:background "#333" :foreground "#fff" :box (:line-width 2 :style released-button)))))
- '(custom-button-mouse ((t (:background "#555" :foreground "#fff" :box (:line-width 2 :style released-button)))))
- '(diff-added ((t (:inherit diff-changed :foreground "green yellow"))))
- '(diff-file-header ((t (:inverse-video t :weight bold))))
- '(diff-hunk-header ((t (:inherit (highlight diff-header)))))
- '(diff-removed ((t (:inherit diff-changed :foreground "tomato"))))
- '(highlight ((t (:background "#202020"))))
- '(ido-first-match ((t (:foreground "#D64937" :weight bold))))
- '(ido-only-match ((t (:foreground "lawn green"))))
- '(ido-subdir ((t (:foreground "#729FCF"))))
- '(mode-line ((t (:background "#d64937" :foreground "black"))))
- '(mode-line-emphasis ((t (:foreground "black" :weight bold))))
- '(mode-line-inactive ((t (:inherit mode-line :background "#222" :foreground "#999" :weight light))))
- '(region ((t (:background "white" :foreground "black")))))
+ )
