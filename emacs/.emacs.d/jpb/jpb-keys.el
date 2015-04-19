57b632acebb48a95591778d072fc825bf99d9d83
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2015-04-19 21:05:07 +0200

    emacs: add function to change casing of identifiers
    CamelCase -> under_score_case -> dash-lispy-case

--- emacs/.emacs.d/jpb/jpb-keys.el
+++ emacs/.emacs.d/jpb/jpb-keys.el
@@ -121,4 +121,42 @@
 
 (global-set-key [f5] 'toggle-php-html-mode)
 
+
+;;
+;; Camelscore
+;;
+;;   http://www.emacswiki.org/emacs/CamelCase
+;;
+
+(defun split-name (s)
+  (split-string
+   (let ((case-fold-search nil))
+     (downcase
+      (replace-regexp-in-string "\\([a-z]\\)\\([A-Z]\\)" "\\1 \\2" s)))
+   "[^A-Za-z0-9]+"))
+
+(defun camelcase  (s) (mapconcat 'capitalize (split-name s) ""))
+(defun underscore (s) (mapconcat 'downcase   (split-name s) "_"))
+(defun dasherize  (s) (mapconcat 'downcase   (split-name s) "-"))
+(defun colonize   (s) (mapconcat 'capitalize (split-name s) "::"))
+
+(defun camelscore (s)
+      (cond ;;((string-match-p "\:"  s) (camelcase s))
+	    ((string-match-p "-" s)   (camelcase s)) ;; (colonize s))
+	    ((string-match-p "_" s)   (dasherize s))
+	    (t                        (underscore s))))
+
+(defun camelscore-word-at-point ()
+  (interactive)
+  (let* ((case-fold-search nil)
+         ;; (beg (and (skip-chars-backward "[:alnum:]:_-") (point)))
+         ;; (end (and (skip-chars-forward  "[:alnum:]:_-") (point)))
+         (beg (and (skip-chars-backward "[:alnum:]_-") (point)))
+         (end (and (skip-chars-forward  "[:alnum:]_-") (point)))
+         (txt (buffer-substring beg end))
+         (cml (camelscore txt)) )
+    (if cml (progn (delete-region beg end) (insert cml)))))
+
+(global-set-key (kbd "C-c a") 'camelscore-word-at-point)
+
 (provide 'jpb-keys)
