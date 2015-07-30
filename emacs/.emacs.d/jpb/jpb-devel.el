5b32f591871d0f62ff831d5b3d59226ecfd7ace0
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2015-07-30 10:03:34 +0200

    emacs: add gtags tweaks

--- emacs/.emacs.d/jpb/jpb-devel.el
+++ emacs/.emacs.d/jpb/jpb-devel.el
@@ -166,20 +166,23 @@
 ;;
 
 (defun gtags-root-dir ()
-    "Returns GTAGS root directory or nil if doesn't exist."
-    (with-temp-buffer
-      (if (zerop (call-process "global" nil t nil "-pr"))
-          (buffer-substring (point-min) (1- (point-max)))
-        nil)))
+  "Returns GTAGS root directory or nil if doesn't exist."
+  (with-temp-buffer
+    (if (zerop (call-process "global" nil t nil "-pr"))
+        (buffer-substring (point-min) (1- (point-max)))
+      nil)))
 
 (defun gtags-update ()
-    "Make GTAGS incremental update"
-    (call-process "global" nil nil nil "-u"))
+  "Make GTAGS incremental update"
+  (interactive)
+  (call-process "global" nil nil nil "-u"))
 
 (defun gtags-update-hook ()
     (when (gtags-root-dir)
       (gtags-update)))
-(add-hook 'after-save-hook #'gtags-update-hook)
+
+;; Not needed, ggtags takes care of this now...
+;;   (add-hook 'after-save-hook #'gtags-update-hook)
 
 (defun compile-gtags (str)
   "compile etags for the current project"
