0bf2ae6e9b585cc797e1d9410b42bbdf9ef3bd56
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2015-10-23 21:47:15 +0200

    emacs: enable web-mode for a lot of things

--- emacs/.emacs.d/jpb/jpb-devel.el
+++ emacs/.emacs.d/jpb/jpb-devel.el
@@ -223,3 +223,16 @@
 
 (add-to-list 'compilation-error-regexp-alist '("^In file \\(.*?\\):\\([0-9]+\\)$" 1 2))
 (add-to-list 'compilation-error-regexp-alist '("^    #[0-9]+ 0x[[:xdigit:]]+ in .* \\(.*?\\):\\([0-9]+\\)$" 1 2))
+
+;;
+;; Web mode
+;;
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
