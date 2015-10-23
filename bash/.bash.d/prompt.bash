8e28d34b9dcb9f9ce38b08a5ebdbbb87b43d0d65
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2015-10-23 21:41:42 +0200

    bash: fancier prompt that showsstatus of last command

--- bash/.bash.d/prompt.bash
+++ bash/.bash.d/prompt.bash
@@ -3,29 +3,44 @@ source ~/.bash.d/git-prompt.sh
 
 set_prompt () {
     lastcmd=$? # Must come first!
+
+    fancyx='\342\234\227'
+    checkmark='\342\234\223'
+
     blue='\[\e[01;34m\]'
-    __blue='\033[34m'
+    blue2='\[\e[00;34m\]'
     white='\[\e[01;37m\]'
     red='\[\e[01;31m\]'
     green='\[\e[01;32m\]'
+    green2='\[\e[00;32m\]'
     reset='\[\e[00m\]'
-    fancyx='\342\234\227'
-    checkmark='\342\234\223'
+
     PS1=""
-    if [[ $EUID == 0 ]]; then
-        PS1+="$red\\h"
+
+    if [[ $lastcmd == 0 ]]; then
+        PS1+="$green$checkmark$reset "
     else
-        PS1+="$green\\u$reset\[$__green\]@\\h"
+        PS1+="$red$fancyx$reset "
     fi
-    PS1+="$reset \[$__blue\]\\w$red"
+
+    if [[ $EUID == 0 ]]; then
+        PS1+="$red\\u$reset"
+    else
+        PS1+="$green2\\u$reset"
+    fi
+
+    PS1+=" $blue2\\w$reset"
+
     if [[ $EUID != 0 ]]; then
-        PS1+='\[`
+        PS1+="`
          case "$(__git_prompt_color)" in
-            red) printf '$__red';;
-            green) printf '$__green';;
-            black) printf '$__black';;
-         esac`\]$(__git_prompt)\[\033[0m\]'
+            red) printf "$red";;
+            green) printf "$green";;
+            black) printf "$white";;
+         esac`$(__git_prompt)$reset"
     fi
+
     PS1+=" "
 }
+
 PROMPT_COMMAND='set_prompt'
