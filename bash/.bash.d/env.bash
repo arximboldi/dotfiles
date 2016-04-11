78d91d3c68a194638fae4db373c8525d4afdb9a9
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2016-04-11 13:01:55 +0200

    bash: update Guix environment

--- bash/.bash.d/env.bash
+++ bash/.bash.d/env.bash
@@ -36,4 +36,7 @@ export NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules
 
 # Guix
 export GUIX_LOCPATH=$HOME/.guix-profile/lib/locale
-add-path /home/raskolnikov/.guix-profile/bin
+export GUILE_LOAD_PATH=$HOME/.guix-profile/share/guile/site/2.0
+export GUILE_LOAD_COMPILED_PATH=$HOME/.guix-profile/share/guile/site/2.0
+add-path $HOME/.guix-profile/bin
+add-path $HOME/.guix-profile/sbin
