19226b12969854d4b85d22ad7173b2f671e15e30
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2016-06-30 19:04:54 +0200

    bash: add variable to make Clojure boot faster

--- bash/.bash.d/env.bash
+++ bash/.bash.d/env.bash
@@ -44,3 +44,6 @@ export GUILE_LOAD_PATH=$HOME/.guix-profile/share/guile/site/2.0
 export GUILE_LOAD_COMPILED_PATH=$HOME/.guix-profile/share/guile/site/2.0
 add-path $HOME/.guix-profile/bin
 add-path $HOME/.guix-profile/sbin
+
+# Clojure
+# export LEIN_FAST_TRAMPOLINE=true
