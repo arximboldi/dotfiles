56317a2ed0ad9adc47c817c39d5537f5960667a0
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2016-08-30 15:09:52 +0200

    bash: cleanup environment file

--- bash/.bash.d/env.bash
+++ bash/.bash.d/env.bash
@@ -8,6 +8,9 @@ export ALTERNATE_EDITOR=""
 export EDITOR="emacsclient -t"
 export VISUAL="emacsclient -c"
 
+#
+# C++
+#
 export LCVER=3.8
 export LC="ccache clang-$LCVER"
 export LXX="ccache clang++-$LCVER"
@@ -20,24 +23,46 @@ export SHLIB_GXXLD="g++-$GCVER"
 # export CCFLAGS="-fdiagnostics-color=always"
 # export CXXFLAGS="-fdiagnostics-color=always"
 
-export GOPATH=~/.go-path
 export CTEST_OUTPUT_ON_FAILURE=1
 
+#
+# Go
+#
+export GOPATH=~/.go-path
 add-path $GOPATH/bin
-add-path /home/raskolnikov/usr/bin
-add-path /home/raskolnikov/.cabal/bin
 
+#
+# Local installs
+#
+add-path ~/usr/bin
+
+#
+# Haskell
+#
+add-path ~/.cabal/bin
+
+#
+# Node.js
+#
 export NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules
 
+#
 # Google cloud stuff
-[ -f /home/raskolnikov/.travis/travis.sh ] && \
-    source ~/soft/google-cloud-sdk/path.bash.inc
+#
 [ -f ~/soft/google-cloud-sdk/completion.bash.inc ] && \
     source ~/soft/google-cloud-sdk/completion.bash.inc
+[ -f ~/soft/google-cloud-sdk/path.bash.inc ] && \
+    source ~/soft/google-cloud-sdk/path.bash.inc
+
+#
+# Travis
+#
 [ -f ~/.travis/travis.sh ] && \
     source ~/.travis/travis.sh
 
+#
 # Guix
+#
 export CPATH=$HOME/.guix-profile/include
 export LIBRARY_PATH=$HOME/.guix-profile/lib
 export GUIX_LOCPATH=$HOME/.guix-profile/lib/locale
@@ -46,7 +71,9 @@ export GUILE_LOAD_COMPILED_PATH=$HOME/.guix-profile/share/guile/site/2.0
 add-path $HOME/.guix-profile/bin
 add-path $HOME/.guix-profile/sbin
 
+#
 # Clojure
+#
 # export LEIN_FAST_TRAMPOLINE=true
 
 #
