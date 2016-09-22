3b78664bbcc6e47f636cd85319f8659453a5260f
jbo <jbo@ableton.com>
2016-09-22 15:26:03 +0200

    emacs: make Emacs For MacOSX happy

--- emacs/.emacs.d/jpb/jpb-macos.el
+++ emacs/.emacs.d/jpb/jpb-macos.el
@@ -6,6 +6,11 @@
 ;;
 
 (setq ls-lisp-use-insert-directory-program t)
-(setq insert-directory-program "gls")
+(setq insert-directory-program "/usr/local/bin/gls")
+
+(setq
+ ns-command-modifier 'meta         ; Apple/Command key is Meta
+ ns-alternate-modifier nil         ; Option is the Mac Option key
+ ns-use-mac-modifier-symbols  nil) ; display standard Emacs modifier symbols
 
 (provide 'jpb-macos)
