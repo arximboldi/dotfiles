e66783aa67a30f594ff5d82e18adfbdd0da69d7a
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2017-02-01 15:46:08 +0100

    emacs: remove lualatex "by default"
    Use Tex-engine variable locally.

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(LaTeX-command "lualatex")
  '(LaTeX-command-style (quote (("" "%(latex) %S%(PDFout)"))))
  '(TeX-output-view-style
    (quote
@@ -170,7 +169,7 @@
  '(send-mail-function nil)
  '(shell-toggle-launch-shell (quote shell))
  '(show-paren-mode t)
- '(sort-fold-case t t)
+ '(sort-fold-case t)
  '(tool-bar-mode nil)
  '(truncate-lines nil)
  '(truncate-partial-width-windows nil)
