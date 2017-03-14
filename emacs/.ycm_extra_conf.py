159527ad1125739affac455cfb2d18516770955e
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2017-03-14 13:46:33 +0100

    emacs: improve YCMD mechanism for finding alternative files

--- emacs/.ycm_extra_conf.py
+++ emacs/.ycm_extra_conf.py
@@ -30,41 +30,58 @@ HEADER_EXTENSIONS = [
     '.hh'
 ]
 
-def is_header_file(filename):
-    extension = os.path.splitext(filename)[1]
-    return extension in HEADER_EXTENSIONS
-
-def get_some_file_from_database(dbpath):
+def find_similar_file_in_database(dbpath, filename):
     import json
-    return json.load(open(dbpath))[0]["file"]
+    import Levenshtein
+    logging.info("Trying to find some file close to: " + filename)
+    db = json.load(open(dbpath))
+    best_filename = ''
+    best_distance = 1 << 31
+    for entry in db:
+        entry_filename = os.path.normpath(
+            os.path.join(entry["directory"], entry["file"]))
+        distance = Levenshtein.distance(str(filename), str(entry_filename))
+        if distance < best_distance:
+            best_filename = entry_filename
+            best_distance = distance
+    return best_filename
+
+def ok_compilation_info(info):
+    return bool(info.compiler_flags_)
 
 def get_compilation_info_for_file(dbpath, database, filename):
-    if is_header_file(filename):
+    info = database.GetCompilationInfoForFile(filename)
+    if ok_compilation_info(info):
+        logging.info("Flags for file where found in database: " + filename)
+        return info
+    else:
+        logging.info("Flags for file not found in database: " + filename)
         basename = os.path.splitext(filename)[0]
         for extension in SOURCE_EXTENSIONS:
             replacement_file = basename + extension
-            if os.path.exists(replacement_file):
-                compilation_info = database.GetCompilationInfoForFile(replacement_file)
-                if compilation_info.compiler_flags_:
-                    return compilation_info
-        return database.GetCompilationInfoForFile(
-            get_some_file_from_database(dbpath))
-    return database.GetCompilationInfoForFile(filename)
+            logging.info("Trying to replace extension with: " + extension)
+            info = database.GetCompilationInfoForFile(replacement_file)
+            if ok_compilation_info(info):
+                logging.info("Replacing header with: " + replacement_file)
+                return info
+        replacement_file = find_similar_file_in_database(dbpath, filename)
+        logging.info("Replacing header with: " + replacement_file)
+        return database.GetCompilationInfoForFile(replacement_file)
 
 def find_nearest(path, target):
-    candidate = os.path.join(path, target)
-    build_candidate = os.path.join(path, 'build', target)
-    if os.path.isfile(candidate) or os.path.isdir(candidate):
-        logging.info("Found nearest " + target + " at " + candidate)
-        return candidate
-    elif os.path.isfile(build_candidate) or os.path.isdir(build_candidate):
-        logging.info("Found nearest " + target + " at " + build_candidate)
-        return build_candidate
-    else:
-        parent = os.path.dirname(os.path.abspath(path))
-        if(parent == path):
-            raise RuntimeError("Could not find " + target)
-        return find_nearest(parent, target)
+    candidates = [
+        os.path.join(path, target),
+        os.path.join(path, 'build', target),
+        os.path.join(path, 'output', target),
+    ]
+    for candidate in candidates:
+        if os.path.isfile(candidate) or os.path.isdir(candidate):
+            logging.info("Found nearest " + target + " at " + candidate)
+            return candidate
+    parent = os.path.dirname(os.path.abspath(path))
+    if parent == path:
+        raise RuntimeError("Could not find " + target)
+    return find_nearest(parent, target)
 
 def make_relative_paths_in_flags_absolute(flags, working_directory):
     if not working_directory:
@@ -74,33 +91,30 @@ def make_relative_paths_in_flags_absolute(flags, working_directory):
     path_flags = [ '-isystem', '-I', '-iquote', '--sysroot=' ]
     for flag in flags:
         new_flag = flag
-
         if make_next_absolute:
             make_next_absolute = False
             if not flag.startswith('/'):
                 new_flag = os.path.join(working_directory, flag)
-
         for path_flag in path_flags:
             if flag == path_flag:
                 make_next_absolute = True
                 break
-
             if flag.startswith(path_flag):
                 path = flag[ len(path_flag): ]
                 new_flag = path_flag + os.path.join(working_directory, path)
                 break
-
         if new_flag:
             new_flags.append(new_flag)
     return new_flags
-
 
 def flags_for_clang_complete(root):
     try:
         clang_complete_path = find_nearest(root, '.clang_complete')
         clang_complete_flags = open(clang_complete_path, 'r').read().splitlines()
         return clang_complete_flags
-    except:
+    except Exception, err:
+        logging.info("Error while looking flags for .clang_complete in root: " + root)
+        logging.error(err)
         return None
 
 def flags_for_include(root):
@@ -112,7 +126,9 @@ def flags_for_include(root):
                 real_path = os.path.join(dirroot, dir_path)
                 flags = flags + ["-I" + real_path]
         return flags
-    except:
+    except Exception, err:
+        logging.info("Error while looking flags for includes in root: " + root)
+        logging.error(err)
         return None
 
 def flags_for_compilation_database(root, filename):
@@ -132,7 +148,9 @@ def flags_for_compilation_database(root, filename):
         return make_relative_paths_in_flags_absolute(
             compilation_info.compiler_flags_,
             compilation_info.compiler_working_dir_)
-    except:
+    except Exception, err:
+        logging.info("Error while trying to get flags for " + filename + " in compilation database")
+        logging.error(err)
         return None
 
 def flags_for_file(filename):
