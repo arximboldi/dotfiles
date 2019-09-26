cf5e978f7c287d6b64a78695138a8a45431ba9b3
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2019-09-26 16:24:09 +0200

    emacs: do not use xterm-color anymore

--- emacs/.emacs.d/init/init-generic.el
+++ emacs/.emacs.d/init/init-generic.el
@@ -51,10 +51,12 @@
 ;;
 ;; Shell
 ;;
-(require 'xterm-color)
-(add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
-(setq comint-output-filter-functions (remove 'ansi-color-process-output comint-output-filter-functions))
-(setq font-lock-unfontify-region-function 'xterm-color-unfontify-region)
+
+;; (require 'xterm-color)
+;; (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
+;; (setq comint-output-filter-functions (remove 'ansi-color-process-output comint-output-filter-functions))
+;; (setq font-lock-unfontify-region-function 'xterm-color-unfontify-region)
+
 (add-hook 'shell-mode-hook
           '(lambda ()
              (toggle-truncate-lines 1)
