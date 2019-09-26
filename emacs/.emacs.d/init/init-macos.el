3f458fe88bc03883e2dd0bdb150abbc21c95e6c9
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2019-09-26 16:31:42 +0200

    emacs: adapt for nixos-darwin

--- emacs/.emacs.d/init/init-macos.el
+++ emacs/.emacs.d/init/init-macos.el
@@ -1,14 +1,14 @@
 ;;; emacs config file -- Juan Pedro Bolívar Puente
 
-(setq ls-lisp-use-insert-directory-program t)
-(setq insert-directory-program "/usr/local/bin/gls")
+;; (setq ls-lisp-use-insert-directory-program t)
+;; (setq insert-directory-program "/usr/local/bin/gls")
 
 (setq
  ns-command-modifier 'meta         ; Apple/Command key is Meta
  ns-alternate-modifier nil         ; Option is the Mac Option key
  ns-use-mac-modifier-symbols  nil) ; display standard Emacs modifier symbols
 
-(set-face-attribute 'default nil
-                    :family "Fira Code" :height 160 :weight 'normal)
+;; (set-face-attribute 'default nil
+;;                     :family "Fira Code" :height 160 :weight 'normal)
 
 (provide 'init-macos)
