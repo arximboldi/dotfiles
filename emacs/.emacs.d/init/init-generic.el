5dd3dc2d7c9cf2c046145243cf6e1b3cae02ff0f
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2022-01-30 14:39:21 +0100

    emacs: add command to randomize order of lines

--- emacs/.emacs.d/init/init-generic.el
+++ emacs/.emacs.d/init/init-generic.el
@@ -257,4 +257,16 @@ Also returns nil if pid is nil."
      (while (search-forward (string ?\C-m) nil t)
        (replace-match (string ?\C-j) nil t))))
 
+(defun @randomize-lines (beg end)
+  "Randomize lines in region from BEG to END."
+  (interactive "*r")
+  (let ((lines (split-string
+                (delete-and-extract-region beg end) "\n")))
+    (when (string-equal "" (car (last lines 1)))
+      (setq lines (butlast lines 1)))
+    (apply 'insert
+           (mapcar 'cdr
+                   (sort (mapcar (lambda (x) (cons (random) (concat x "\n"))) lines)
+                         (lambda (a b) (< (car a) (car b))))))))
+
 (provide 'init-generic)
