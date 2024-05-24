67d7ccd63b4cf80059884cc2bacac56f13203791
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2024-05-24 14:01:38 +0200

    emcs: fix shell colors by using latest xterm-colors incarnation

--- emacs/.emacs.d/init/init-generic.el
+++ emacs/.emacs.d/init/init-generic.el
@@ -54,7 +54,22 @@
 ;; Shell
 ;;
 
-;; (require 'xterm-color)
+(require 'xterm-color)
+(setq comint-output-filter-functions
+      (remove 'ansi-color-process-output comint-output-filter-functions))
+(add-hook 'shell-mode-hook
+          (lambda ()
+            ;; Disable font-locking in this buffer to improve performance
+            (font-lock-mode -1)
+            ;; Prevent font-locking from being re-enabled in this buffer
+            (make-local-variable 'font-lock-function)
+            (setq font-lock-function (lambda (_) nil))
+            (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter nil t)))
+(setq compilation-environment '("TERM=xterm-256color"))
+(defun my/advice-compilation-filter (f proc string)
+  (funcall f proc (xterm-color-filter string)))
+(advice-add 'compilation-filter :around #'my/advice-compilation-filter)
+
 ;; (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
 ;; (setq comint-output-filter-functions (remove 'ansi-color-process-output comint-output-filter-functions))
 ;; (setq font-lock-unfontify-region-function 'xterm-color-unfontify-region)
