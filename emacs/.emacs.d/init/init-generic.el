611cbba4f9bc7ae9e043ba131fa1d8075b3b32f5
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2024-05-24 13:50:59 +0200

    emacs: comment old code that I think we don't use

--- emacs/.emacs.d/init/init-generic.el
+++ emacs/.emacs.d/init/init-generic.el
@@ -59,12 +59,12 @@
 ;; (setq comint-output-filter-functions (remove 'ansi-color-process-output comint-output-filter-functions))
 ;; (setq font-lock-unfontify-region-function 'xterm-color-unfontify-region)
 
-(ignore-errors
-  (require 'ansi-color)
-  (defun @colorize-compilation-buffer ()
-    (when (eq major-mode 'compilation-mode)
-      (ansi-color-apply-on-region compilation-filter-start (point-max))))
-  (add-hook 'compilation-filter-hook '@colorize-compilation-buffer))
+;;(ignore-errors
+;;  (require 'ansi-color)
+;;  (defun @colorize-compilation-buffer ()
+;;    (when (eq major-mode 'compilation-mode)
+;;      (ansi-color-apply-on-region compilation-filter-start (point-max))))
+;;  (add-hook 'compilation-filter-hook '@colorize-compilation-buffer))
 
 (add-hook 'shell-mode-hook
           '(lambda ()
