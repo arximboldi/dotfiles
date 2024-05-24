56f48939913b05c5b1d5fd9e0e718e77dab29ddc
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2024-05-24 14:05:06 +0200

    nix: tweak nvidia settings to try enable vulkan

--- nix/os/ce2/configuration.nix
+++ nix/os/ce2/configuration.nix
@@ -24,6 +24,22 @@
   nixpkgs.config.allowUnfree = true;
   services.xserver.videoDrivers = [ "nvidia" ];
 
+  # https://nixos.wiki/wiki/Nvidia
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+
+    # Enable the Nvidia settings menu,
+	  # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver
+    # version for your specific GPU.
+    # package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+  };
+
   services.ssh-phone-home = {
     enable = true;
     localUser = "raskolnikov";
