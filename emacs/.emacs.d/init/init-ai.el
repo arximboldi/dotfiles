e4cb7ed25262e71dc650c4ae32add6b2673a1d3b
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2025-08-31 13:57:04 +0200

    emacs: add more tools for ai-coding

--- emacs/.emacs.d/init/init-ai.el
+++ emacs/.emacs.d/init/init-ai.el
@@ -39,7 +39,7 @@
   (define-key gptel-mode-map (kbd "C-c M-n") #'gptel-history-next)
   (define-key gptel-mode-map (kbd "C-c M-p") #'gptel-history-previous)
 
-  (global-set-key (kbd "C-c C-g") 'gptel-send)
+  (global-set-key (kbd "C-c g g") #'gptel-send)
 
   ;; Global keybindings for various GPTel commands
   (global-set-key (kbd "C-c g c") #'gptel)             ;; Open GPTel (or switch to it)
@@ -53,8 +53,39 @@
 (use-package elysium)
 (use-package magit-gptcommit) ;; doesn't support 'gptel on main branch yet...
 
-(use-package aider
-  :config
-  (global-set-key (kbd "C-c a") 'aider-transient-menu))
+;;(use-package aider
+;;  :config
+;;  (global-set-key (kbd "C-c a") 'aider-transient-menu))
+
+(use-package copilot
+  :ensure t
+  :hook (prog-mode . copilot-mode))
+
+(require 'copilot)
+;;(define-key copilot-completion-map (kbd "C-<tab>") 'copilot-accept-completion)
+(define-key copilot-completion-map (kbd "M-p") 'copilot-accept-completion)
+(define-key copilot-completion-map (kbd "M-]") 'copilot-next-completion)
+(define-key copilot-completion-map (kbd "M-[") 'copilot-previous-completion)
+(define-key copilot-completion-map (kbd "M-o") 'copilot-clear-overlay)
+;;(define-key copilot-completion-map (kbd "C-c C-c") 'copilot-clear-overlay)
+(global-set-key (kbd "M-P") 'copilot-panel-complete)
+
+(add-to-list 'copilot-indentation-alist '(prog-mode 4))
+(add-to-list 'copilot-indentation-alist '(org-mode 2))
+(add-to-list 'copilot-indentation-alist '(text-mode 2))
+(add-to-list 'copilot-indentation-alist '(closure-mode 2))
+(add-to-list 'copilot-indentation-alist '(emacs-lisp-mode 2))
+
+;; (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
+;; (define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
+;; (define-key copilot-completion-map (kbd "C-TAB") 'copilot-accept-completion-by-word))
+;; (define-key copilot-completion-map (kbd "C-<tab>") 'copilot-accept-completion-by-word))
+;; (define-key copilot-completion-map (kbd "C-n") 'copilot-next-completion))
+;; (define-key copilot-completion-map (kbd "C-p") 'copilot-previous-completion))
+
+;;(use-package claude-code :ensure t
+;;  :vc (:url "https://github.com/stevemolitor/claude-code.el" :rev :newest)
+;;  :config (claude-code-mode)
+;;  :bind-keymap ("C-c c" . claude-code-command-map))
 
 (provide 'init-ai)
