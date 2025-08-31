b367ec6e380ae731bf861ea43a3ea751ad6ae6e1
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2025-08-31 14:05:53 +0200

    nix: upgrade to latest musnix

--- nix/os/common/musnix.nix
+++ nix/os/common/musnix.nix
@@ -1,13 +1,9 @@
 { config, pkgs, ... }:
 
 let
-
-  fetchFromGitHub = (import <nixpkgs> {}).fetchFromGitHub;
-  musnix-src = fetchFromGitHub {
-    owner  = "musnix";
-    repo   = "musnix";
-    rev    = "86ef22cbdd7551ef325bce88143be9f37da64c26";
-    sha256 = "sha256-lvee0rhKTpJEDl1SC4F3Kvz88snOU8OMTJBsPH1pVBo=";
+  musnix-src = builtins.fetchGit {
+    url  = "https://github.com/musnix/musnix.git";
+    rev  = "86ef22cbdd7551ef325bce88143be9f37da64c26";
   };
 
 in
