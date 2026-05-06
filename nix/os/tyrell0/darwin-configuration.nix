3b9d462a293efced34c2d0dfabc9d38eaf648bbd
nixmac <nixmac@local>
2026-05-06 10:40:37 +0200

    nix: add iterm2

--- nix/os/tyrell0/darwin-configuration.nix
+++ nix/os/tyrell0/darwin-configuration.nix
@@ -5,7 +5,7 @@
  # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-    # iterm2
+    iterm2
     slack
     gdb
     # valgrind
