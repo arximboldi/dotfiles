78dbad54f9e71ba6ca3613ecb63782a6ac34c611
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2026-09-05 14:19:22 +0200

    sops bootstrap

--- nix/os/tyrell0/darwin-configuration.nix
+++ nix/os/tyrell0/darwin-configuration.nix
@@ -12,6 +12,8 @@ let
   };
 in
 {
+  # sops.secrets = { };
+
  # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
